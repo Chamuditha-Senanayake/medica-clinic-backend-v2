cfc6436136f5b8e4fab996a66a1c3d677bf66221
chamcbs <chamudithacbs2@gmail.com>
2024-01-08 19:30:45 +0530

    saveNurse service

--- features/nurse/controllers/nurse.controller.js
+++ features/nurse/controllers/nurse.controller.js
@@ -65,6 +65,83 @@ const NurseController = {
       next(error);
     }
   },
+
+  /**
+   *
+   * save a nurse
+   *
+   * @param {request} request object
+   * @param {response} response object
+   * @param {next} next function
+   * @returns
+   */
+  async saveNurse(request, response, next) {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(422).json({
+        error: true,
+        message: ResponseMessage.Nurse.VALIDATION_ERROR,
+        data: errors,
+      });
+    }
+
+    try {
+      let connection = request.app.locals.db;
+      const {
+        Id,
+        FirstName,
+        MiddleName,
+        LastName,
+        Email,
+        NIC,
+        Title,
+        Status,
+        UserSaved,
+      } = request.body;
+
+       var params = [
+      EntityId({ fieldName: "Id", value: Id }),
+      StringValue({ fieldName: "FirstName", value: FirstName }),
+      StringValue({ fieldName: "MiddleName", value: MiddleName }),
+      StringValue({ fieldName: "LastName", value: LastName }),
+      StringValue({ fieldName: "Email", value: Email }),
+      StringValue({ fieldName: "NIC", value: NIC }),
+      StringValue({ fieldName: "Title", value: Title }),
+      SignedInteger({
+        fieldName: "Status",
+        value: Status,
+      }),
+      EntityId({ fieldName: "UserSaved", value: UserSaved }),
+    ];
+
+      let nurseSaveResult = await executeSp({
+        spName: `NurseSave`,
+        params: params,
+        connection,
+      });
+
+      console.log(nurseSaveResult.recordsets);
+      nurseSaveResult = nurseSaveResult.recordsets;
+
+      handleResponse(
+        response,
+        200,
+        "success",
+        "Bill data retrieved successfully",
+        nurseSaveResult
+      );
+    } catch (error) {
+      handleError(
+        response,
+        500,
+        "error",
+        error.message,
+        "Something went wrong"
+      );
+      next(error);
+    }
+  },
+
 };
 
 export default NurseController;
--- features/nurse/routes/nurse.route.js
+++ features/nurse/routes/nurse.route.js
@@ -13,4 +13,20 @@ router.post(
   NurseController.getNurse
 );
 
+router.post(
+  "/NurseSave",
+  [
+    check("Id").not().isEmpty(),
+    check("FirstName").not().isEmpty(),
+    check("MiddleName").not().isEmpty(),
+    check("LastName").not().isEmpty(),
+    check("Email").not().isEmpty().isEmail(),    
+    check("NIC").not().isEmpty(),
+    check("Title").not().isEmpty(),
+    check("Status").not().isEmpty(),
+    check("UserSaved").not().isEmpty(),
+  ],
+  NurseController.saveNurse
+);
+
 export default router;

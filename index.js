eb9b3fa6b2b66a621119ca901d3f8c19d31c59e3
chamcbs <chamudithacbs2@gmail.com>
2024-01-31 14:56:28 +0530

    complete note services

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import nurseRouter from "./features/nurse/routes/nurse.route.js";
 import prescriptionRouter from "./features/prescription/routes/prescription.route.js";
 import diseaseRouter from "./features/disease/routes/disease.route.js";
 import patientRouter from "./features/patient/routes/patient.route.js";
+import noteRouter from "./features/note/routes/note.route.js";
 
 
 dotenv.config();
@@ -46,6 +47,7 @@ app.use(`/api/v1`, nurseRouter);
 app.use(`/api/v1`, prescriptionRouter);
 app.use(`/api/v1`, diseaseRouter);
 app.use(`/api/v1`, patientRouter);
+app.use(`/api/v1`, noteRouter);
 
 // set port, listen for requests
 const APP_PORT = process.env.APP_PORT;

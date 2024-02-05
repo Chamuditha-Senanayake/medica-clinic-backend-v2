ad69d9184618471e677dc4cd167dc7286d24312f
chamcbs <chamudithacbs2@gmail.com>
2024-02-05 19:55:17 +0530

    add patient services

--- features/patient/routes/patient.route.js
+++ features/patient/routes/patient.route.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.post(
   "/PatientDiagnosisDocumentGet",
   [
-    check("Id").not().isEmpty(),
-     check("PatientId").not().isEmpty(),
-    check("UserId").not().isEmpty(),
+    check("Id").isInt().not().isEmpty(),
+     check("PatientId").isInt().not().isEmpty(),
+    check("UserId").isInt().not().isEmpty(),
   ],
   PatientController.getPatientDiagnosisDocument
 );
@@ -25,5 +25,61 @@ router.post(
   ],
   PatientController.savePatientDiagnosisDocument
 );
+
+router.post(
+  "/PatientDiseaseSave",
+  [
+    check("PatientId").isInt().not().isEmpty(),
+    check("UserSaved").isInt().not().isEmpty(),
+    check("PatientDisease").not().isEmpty(),
+  ],
+  PatientController.savePatientDisease
+);
+
+router.post(
+  "/PatientDispositionGet",
+  [
+    check("Id").not().isEmpty(),
+    check("UserId").not().isEmpty(),
+  ],
+  PatientController.getPatientDisposition
+);
+
+router.post(
+  "/PatientDispositionSave",
+  [
+    check("AppointmentId").isInt().not().isEmpty(),
+    check("PrescriptionId").isInt().not().isEmpty(),
+    check("PatientId").isInt().not().isEmpty(),
+    check("DoctorId").isInt().not().isEmpty(),
+    check("InstituteId").isInt().not().isEmpty(),
+    check("DispositionId").isInt().not().isEmpty(),
+    check("DispositionTypeId").isInt().not().isEmpty(),
+    check("AppointmentId").isInt().not().isEmpty(),
+    check("ServiceTypeId").isInt().not().isEmpty(),
+
+    check("DispositionValue").not().isEmpty(),
+    check("Note").not().isEmpty(),
+    check("NextVisitOption").not().isEmpty(),
+    check("ReminderType").not().isEmpty(),
+    check("ReminderMessage").not().isEmpty(),
+
+    check("UserSaved").isInt().not().isEmpty(),
+    check("Id").isInt().not().isEmpty(),
+    check("UserId").isInt().not().isEmpty(),
+  ],
+  PatientController.savePatientDisposition
+);
+
+router.post(
+  "/PatientDrugAllergyGet",
+  [
+    check("Id").isInt().not().isEmpty(),
+    check("PatientId").isInt().not().isEmpty(),
+    check("UserId").isInt().not().isEmpty(),
+  ],
+  PatientController.getPatientDrugAllergy
+);
+
 
 export default router;

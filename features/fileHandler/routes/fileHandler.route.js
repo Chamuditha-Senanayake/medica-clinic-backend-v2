cc94f3b2398e82f192013b848aefff994beb37ed
chamcbs <chamudithacbs2@gmail.com>
2024-06-05 14:06:29 +0530

    changes

--- features/fileHandler/routes/fileHandler.route.js
+++ features/fileHandler/routes/fileHandler.route.js
@@ -3,27 +3,11 @@ import { check } from 'express-validator';
 // import FileHandlerController from '../controllers/fileHandler.controller.js';
 import upload from '../../../middleware/upload.middleware.js';
 import { isAuth } from '../../../middleware/auth.middleware.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const router = express.Router();
-
-// router.post(
-//   '/NoteGet',
-//   [
-//     check('Id').isInt().not().isEmpty(),
-//     check('PatientId').isInt().not().isEmpty(),
-//     check('UserId').not().isEmpty(),
-//   ],
-//   NoteController.getNote
-// );
 
 router.post('/FileUpload', isAuth, upload.single('File'), (req, res) => {
   try {
-    const resourceName = req.ResourceName;
     const file = req.file;
 
     if (!file) {

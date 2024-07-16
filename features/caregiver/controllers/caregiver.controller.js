7b556d296573d6c215e31965f04250930afbad23
chamcbs <chamudithacbs2@gmail.com>
2024-07-16 15:18:38 +0530

    email changes

--- features/caregiver/controllers/caregiver.controller.js
+++ features/caregiver/controllers/caregiver.controller.js
@@ -108,7 +108,7 @@ const CaregiverController = {
             sendEmailFromCustomAccount({
               to: CaregiverEmail,
               subject: 'You have assigned as a caregiver',
-              html: `<p>Hello ${CaregiverName},</p><h2>Verify Your Email</h2><p>Step 1 - Signup </p><a href='${process.env.FRONTEND_URL}/signup'> here</a> 
+              html: `<p>Hello ${CaregiverName},</p><h2>Verify Your Email</h2><p>Step 1 - Signup </p><a href='${process.env.FRONTEND_URL}/signup'> ${process.env.FRONTEND_URL}/signup</a> 
               <p>Step 2 - Accept invitation </p><a href='${process.env.FRONTEND_URL}/invitation/caregiver?token=${token}'> ${process.env.FRONTEND_URL}/invitation/caregiver?token=${token}</a>`,
             });
           } else {

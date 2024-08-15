3bdafdd6063902cb786662da33859de2ddbc297d
chamcbs <chamudithacbs2@gmail.com>
2024-08-15 11:39:22 +0530

    admin and superAdmin middleware

---
+++ middleware/admin.middleware.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import { EntityId } from '../utils/type-def.js';
+import executeSp from '../utils/exeSp.js';
+
+export const isAdmin = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  const token = authorization.slice(7, authorization.length);
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
+    if (err) {
+      res.status(401).send({ message: 'Unauthorized' });
+    } else {
+      req.user = decode;
+
+      if (req.user.userId) {
+        let connection = req.app.locals.db;
+
+        var params = [EntityId({ fieldName: 'Id', value: req.user.userId })];
+        try {
+          let userData = await executeSp({
+            spName: `UserGetById`,
+            params: params,
+            connection,
+          });
+
+          if (!userData) {
+            throw Error('User not found');
+          } else if (
+            userData.recordsets[0][0].UserRole === 1 ||
+            userData.recordsets[0][0].UserRole === 2
+          ) {
+            next();
+          } else {
+            res.status(401).send({
+              message: 'You are not allowed to perform this action',
+            });
+          }
+        } catch (error) {
+          res.status(401).send({ message: 'Failed to perform this action' });
+        }
+      }
+    }
+  });
+};

65d4b5e18d03a01858c6f66ce53b86fbe0502f7d
LasithEranga <lasitheranga1@gmail.com>
2024-08-30 12:05:05 +0530

    fix validation

--- features/institute/routes/institute.route.js
+++ features/institute/routes/institute.route.js
@@ -7,8 +7,8 @@ router.post(
   "/InstituteDoctor/GetInstituteByDoctor",
   [
     check("Id").isInt().not().isEmpty(),
-    check("InstituteBranchId").isInt().not().isEmpty(),
-    check("UserId").not().isEmpty(),
+    // check("InstituteBranchId").isInt().not().isEmpty(),
+    // check("UserId").not().isEmpty(),
   ],
   InstituteController.getInstituteBranchDoctor
 );
@@ -28,10 +28,10 @@ router.post(
 router.post(
   "/InstituteBranch/GetInstituteBranch",
   [
-    check("Id").isInt().not().isEmpty(),
+    // check("Id").isInt().not().isEmpty(),
     check("DoctorId").isInt().not().isEmpty(),
-    check("InstituteId").isInt().not().isEmpty(),
-    check("UserId").not().isEmpty(),
+    // check("InstituteId").isInt().not().isEmpty(),
+    // check("UserId").not().isEmpty(),
   ],
   InstituteController.getInstituteBranch
 );

6c4dcc520db49cbc339d96bedb10b739602d0ec7
chamcbs <chamudithacbs2@gmail.com>
2024-09-10 14:26:05 +0530

    create logger middleware

--- middleware/logger.middleware.js
+++ middleware/logger.middleware.js
@@ -2,12 +2,26 @@ import { info } from '../config/logger.js';
 
 export default function loggerMiddleware(request, response, next) {
   info(`Start processing the request(loggerMiddleware)`);
-  const newObj = {};
+  const newObj = {
+    body: request.body,
+    query: request.query,
+    params: request.params,
+  };
+  console.log(request.path);
   info(
     `Request Method: ${request.method} | Request Path: ${
       request.path
     } | Request Data: ${JSON.stringify(newObj)}`
   );
 
-  return next();
+  info(
+    `Request: ${request.method} ${request.path}\n${JSON.stringify({
+      path: request.path,
+      method: request.method,
+      body: request.body,
+      query: request.query,
+      params: request.params,
+    })}`
+  );
+  next();
 }

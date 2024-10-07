d2d035686c7634f34e9ba75ce793ec7e628bb4f0
LasithEranga <lasitheranga1@gmail.com>
2024-10-07 14:27:50 +0530

    path fix

--- features/doctor/routes/doctor-routes.js
+++ features/doctor/routes/doctor-routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import {
-  assignSessionCategoryToDoctor, doctorServiceFeeGet, doctorServiceFeeSave, getByBranch, getByUser,
-  getDoctorBio, getDoctorByInstitute, getDoctorInstitute,
-  getDoctorSessionCategories, notifyNewAppointment,
+  assignSessionCategoryToDoctor,
+  doctorServiceFeeGet,
+  doctorServiceFeeSave,
+  getByBranch,
+  getByUser,
+  getDoctorBio,
+  getDoctorByInstitute,
+  getDoctorInstitute,
+  getDoctorSessionCategories,
+  notifyNewAppointment,
   saveDoctorSpecialization,
   saveOrUpdateDoctor,
   setDoctorBio,
@@ -17,26 +24,28 @@ doctorRoutes.post(
   "/set-show-doctor-for-online-booking",
   setShowDoctorForOnlineBookingStatus
 );
-doctorRoutes.post("/assign-doctor-session-category", assignSessionCategoryToDoctor);
+doctorRoutes.post(
+  "/assign-doctor-session-category",
+  assignSessionCategoryToDoctor
+);
 doctorRoutes.post("/get-doctor-session-categories", getDoctorSessionCategories);
 
 doctorRoutes.post("/save-or-update", saveOrUpdateDoctor);
 
 doctorRoutes.post("/save-specializations", saveDoctorSpecialization);
 
-doctorRoutes.post(`/doctor/get-by-user`, getByUser);
+doctorRoutes.post(`/get-by-user`, getByUser);
 
-doctorRoutes.post(`/doctor/get-by-branch`, getByBranch);
+doctorRoutes.post(`/get-by-branch`, getByBranch);
 
 doctorRoutes.post(`/doctor-service-fee/save`, doctorServiceFeeSave);
 
 doctorRoutes.get(`/doctor-service-fee/get/`, doctorServiceFeeGet);
 
-doctorRoutes.post(`/doctor/notify-new-appointment`, notifyNewAppointment);
+doctorRoutes.post(`/notify-new-appointment`, notifyNewAppointment);
 
-doctorRoutes.post(`/doctor/by-institute/get/`, getDoctorByInstitute);
+doctorRoutes.post(`/by-institute/get/`, getDoctorByInstitute);
 
-doctorRoutes.post(`/doctor/institute/get/`, getDoctorInstitute);
-
+doctorRoutes.post(`/institute/get/`, getDoctorInstitute);
 
 export default doctorRoutes;

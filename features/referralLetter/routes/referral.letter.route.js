cc65e0f2c2e9bac8626e21b8234a88bfa3a90b71
LasithEranga <lasitheranga1@gmail.com>
2024-10-11 16:37:50 +0530

    fix

--- features/referralLetter/routes/referral.letter.route.js
+++ features/referralLetter/routes/referral.letter.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { check } from "express-validator";
-import MedicalCertificateController from "../controllers/medicalCertificate.controller.js";
 import ReferralLetterController from "../controller/referral.letter.controller.js";
 const router = express.Router();
 

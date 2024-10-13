3d38b7eb3e131ed7049e1fd3af82ed0a765e858c
LasithEranga <lasitheranga1@gmail.com>
2024-10-13 19:38:33 +0530

    referral letter save api

--- features/referralLetter/controller/referral.letter.controller.js
+++ features/referralLetter/controller/referral.letter.controller.js
@@ -3,7 +3,11 @@ import ResponseMessage from "../../../config/messages.js";
 import executeSp from "../../../utils/exeSp.js";
 import handleError from "../../../utils/handleError.js";
 import handleResponse from "../../../utils/handleResponse.js";
-import { EntityId, StringValue } from "../../../utils/type-def.js";
+import {
+  EntityId,
+  SignedInteger,
+  StringValue,
+} from "../../../utils/type-def.js";
 
 const ReferralLetterController = {
   /**
@@ -38,6 +42,143 @@ const ReferralLetterController = {
 
       let referralLetterTemplateGetResult = await executeSp({
         spName: `TemplateGet`,
+        params: params,
+        connection,
+      });
+
+      referralLetterTemplateGetResult =
+        referralLetterTemplateGetResult.recordsets[0];
+
+      handleResponse(
+        response,
+        200,
+        "success",
+        "Data retrived successfully",
+        referralLetterTemplateGetResult
+      );
+    } catch (error) {
+      handleError(
+        response,
+        500,
+        "error",
+        error.message,
+        "Something went wrong"
+      );
+      next(error);
+    }
+  },
+
+  async saveReferralLetter(request, response, next) {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(422).json({
+        error: true,
+        message: ResponseMessage.MedicalCertificate.VALIDATION_ERROR,
+        data: errors,
+      });
+    }
+
+    try {
+      let connection = request.app.locals.db;
+      const {
+        Id,
+        AppointmentId,
+        AppointmentNumber,
+        AppointmentSessionId,
+        IssuingDate,
+        Message,
+        PatientBloodGroup,
+        PatientDateOfBirth,
+        PatientFirstName,
+        PatientId,
+        PatientLastName,
+        PatientMiddleName,
+        PatientMobile,
+        PatientNIC,
+        PatientPassport,
+        PatientPatientTypeId,
+        PatientStatus,
+        PrescriptionId,
+        ReferringDoctor,
+        Status,
+        UserId,
+      } = request.body;
+
+      var params = [
+        EntityId({ fieldName: "TypeId", value: TypeId }),
+        SignedInteger({
+          fieldName: "AppointmentNumber",
+          value: AppointmentNumber,
+        }),
+        EntityId({
+          fieldName: "AppointmentId",
+          value: AppointmentId,
+        }),
+        EntityId({
+          fieldName: "AppointmentSessionId",
+          value: AppointmentSessionId,
+        }),
+        StringValue({ fieldName: "IssuingDate", value: IssuingDate }),
+        StringValue({ fieldName: "Message", value: Message }),
+        StringValue({
+          fieldName: "PatientBloodGroup",
+          value: PatientBloodGroup,
+        }),
+        StringValue({
+          fieldName: "PatientDateOfBirth",
+          value: PatientDateOfBirth,
+        }),
+        StringValue({
+          fieldName: "PatientFirstName",
+          value: PatientFirstName,
+        }),
+        StringValue({
+          fieldName: "PatientId",
+          value: PatientId,
+        }),
+        StringValue({
+          fieldName: "PatientLastName",
+          value: PatientLastName,
+        }),
+        StringValue({
+          fieldName: "PatientMiddleName",
+          value: PatientMiddleName,
+        }),
+        StringValue({
+          fieldName: "PatientMobile",
+          value: PatientMobile,
+        }),
+        StringValue({
+          fieldName: "PatientNIC",
+          value: PatientNIC,
+        }),
+        StringValue({
+          fieldName: "PatientPassport",
+          value: PatientPassport,
+        }),
+        EntityId({
+          fieldName: "PatientPatientTypeId",
+          value: PatientPatientTypeId,
+        }),
+        SignedInteger({
+          fieldName: "PatientStatus",
+          value: PatientStatus,
+        }),
+        EntityId({ fieldName: "PrescriptionId", value: PrescriptionId }),
+        StringValue({
+          fieldName: "ReferringDoctor",
+          value: ReferringDoctor,
+        }),
+        SignedInteger({
+          fieldName: "Status",
+          value: Status,
+        }),
+        EntityId({ fieldName: "UserSaved", value: UserId }),
+        EntityId({ fieldName: "Id", value: Id }),
+      ];
+
+      let referralLetterTemplateGetResult = await executeSp({
+        spName: `ReferralLetterSave`,
         params: params,
         connection,
       });
--- features/referralLetter/routes/referral.letter.route.js
+++ features/referralLetter/routes/referral.letter.route.js
@@ -9,4 +9,34 @@ router.post(
   ReferralLetterController.getReferralLetterTemplate
 );
 
+//    Status,
+//    UserId,
+router.post(
+  "/ReferralLetter/post",
+  [
+    check("Id").optional().isInt(),
+    check("AppointmentId").optional().isInt(),
+    check("AppointmentNumber").optional().not().isEmpty(),
+    check("AppointmentSessionId").optional().isInt(),
+    check("IssuingDate").optional().isString(),
+    check("Message").optional().isString(),
+    check("PatientBloodGroup").optional().isString(),
+    check("PatientDateOfBirth").optional().isString(),
+    check("PatientFirstName").optional().isString(),
+    check("PatientId").optional().isInt(),
+    check("PatientLastName").optional().isString(),
+    check("PatientMiddleName").optional().isString(),
+    check("PatientNIC").optional().isString(),
+    check("PatientPassport").optional().isString(),
+    check("PatientPatientTypeId").optional().isInt(),
+    check("PatientStatus").optional().isInt(),
+    check("PrescriptionId").optional().isInt(),
+    check("ReferringDoctor").optional().isString(),
+    check("PatientMobile").optional().isString(),
+    check("Status").optional().isInt(),
+    check("UserId").optional().isInt(),
+  ],
+  ReferralLetterController.getReferralLetterTemplate
+);
+
 export default router;

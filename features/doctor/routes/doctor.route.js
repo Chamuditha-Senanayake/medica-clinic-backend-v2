e8031e5670711a6407cfef65d5e4f775c775db89
LasithEranga <lasitheranga1@gmail.com>
2024-10-15 23:06:46 +0530

    fix doctor save

--- features/doctor/routes/doctor.route.js
+++ features/doctor/routes/doctor.route.js
@@ -22,7 +22,7 @@ router.post(
     check("LastName").not().isEmpty().isString().isLength({ max: 50 }),
     check("Email").not().isEmpty().isEmail().isLength({ max: 100 }),
     check("NIC").not().isEmpty().isString().isLength({ max: 20 }),
-    check("Status").not().isEmpty().isInt(),
+    check("Status").optional().isInt(),
     check("UserSaved").optional({ values: "null" }).isInt(),
     check("RegistrationNumber")
       .not()
@@ -41,56 +41,9 @@ router.post(
       .isString()
       .withMessage("ZoomPassword must be a string if provided")
       .isLength({ max: 150 }),
-    check("DoctorFee").not().isEmpty().isFloat(),
-    check("HospitalFee").not().isEmpty().isFloat(),
-    check("OtherFee").not().isEmpty().isFloat(),
-    // check("ContactNumbers")
-    //   .isArray()
-    //   .withMessage("ContactNumbers must be an array")
-    //   .notEmpty()
-    //   .withMessage("ContactNumbers array cannot be empty")
-    //   .custom((contactNumbersArray) => {
-    //     for (const contact of contactNumbersArray) {
-    //       if (typeof contact !== "object" || contact === null) {
-    //         throw new Error("Each item in ContactNumbers must be an object");
-    //       }
-
-    //       if (
-    //         !("Number" in contact) ||
-    //         !("Status" in contact) ||
-    //         !("Id" in contact)
-    //       ) {
-    //         throw new Error(
-    //           "Each item in ContactNumbers must have 'Number' and 'Status' and 'Id'"
-    //         );
-    //       }
-
-    //       if (
-    //         typeof contact.Number !== "string" &&
-    //         contact.Number.length > 15
-    //       ) {
-    //         throw new Error("Contact Number must be a string");
-    //       }
-
-    //       if (typeof contact.Status !== "number") {
-    //         throw new Error("Contact Status must be a number");
-    //       }
-
-    //       if (contact.Id) {
-    //         if (typeof contact.Id !== "number") {
-    //           throw new Error("Contact Id must be a number");
-    //         }
-    //       }
-
-    //       // Example: Ensure that the Status is either 0 or 1
-    //       // TODO: should implement in future
-    //       // if (![0, 1].includes(contact.Status)) {
-    //       //   throw new Error("Contact Status must be either 0 or 1");
-    //       // }
-    //     }
-
-    //     return true;
-    //   }),
+    check("DoctorFee").optional().isFloat(),
+    check("HospitalFee").optional().isFloat(),
+    check("OtherFee").optional().isFloat(),
   ],
   DoctorController.saveDoctor
 );
